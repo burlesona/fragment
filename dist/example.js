2a3942dd135004febe3f04465964cb2e8e511efd
Andrew Burleson <andrew.burleson@citrusbyte.com>
2014-05-09 15:19:45 -0400

    Beefed up the example file

--- dist/example.js
+++ dist/example.js
@@ -4,12 +4,22 @@
 
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
-  template = "h1{\"data-test\":\"example\"} Hello World\n\np\n  ' I'm an example of some content.\n  = @message\n\n.test\n  ' I can have strings inside\n\n.one\n  .two\n    .three\n      p Or be deeply nested";
+  template = "h1{\"data-test\":\"example\"}\n  ' Hello World\n  small Meet Fragment\n\np\n  ' Fragment is a dynamic, logic-less templating system that\n  | converts a very terse syntax into living dom nodes.\n\np\n  | Control whitespace using ' or |:\n  ul\n    li = @pipeMessage\n    li = @aposMessage\n\np The syntax looks a lot like Slim or Jade. define a node as follows:\n\ncode\n  | <selector>{<attributes>} <directive> <content>\n\np You can define tags with CSS selectors like:\n\ncode\n  | p#myID.firstClass.secondClass Howdy!\n\np You can skip the tag name for divs:\n\ncode\n  | .classy I'm a classy div!\n  br\n  | div I'm a div with no class!\n\n.nodes\n  p\n    ' My coolest feature, though, is this:\n    ' You can create\n    b living dom nodes&nbsp;\n    ' and insert them into a template.\n    ' These dom nodes will retain their event bindings,\n    ' meaning you can declare your events on the node\n    ' before rendering and they'll never get trashed\n    ' in a re-render.\n\n  p == @node()\n\n  p Pretty cool huh?";
 
   root.init = function() {
     var f;
     f = Fragment.parse(template, {
-      message: "Hello Interpolation!"
+      pipeMessage: "| creates text nodes with no trailing space.",
+      aposMessage: "' creates text nodes with a trailing space.",
+      node: function() {
+        var b;
+        b = document.createElement('button');
+        b.innerText = "Click me and check the console!";
+        b.addEventListener('click', function() {
+          return console.log("You clicked my node!");
+        });
+        return b;
+      }
     });
     return document.body.appendChild(f);
   };
--- src/example.coffee
+++ src/example.coffee
@@ -1,21 +1,62 @@
 root = exports ? this
 
 template = """
-h1{"data-test":"example"} Hello World
+h1{"data-test":"example"}
+  ' Hello World
+  small Meet Fragment
 
 p
-  ' I'm an example of some content.
-  = @message
+  ' Fragment is a dynamic, logic-less templating system that
+  | converts a very terse syntax into living dom nodes.
 
-.test
-  ' I can have strings inside
+p
+  | Control whitespace using ' or |:
+  ul
+    li = @pipeMessage
+    li = @aposMessage
 
-.one
-  .two
-    .three
-      p Or be deeply nested
+p The syntax looks a lot like Slim or Jade. define a node as follows:
+
+code
+  | <selector>{<attributes>} <directive> <content>
+
+p You can define tags with CSS selectors like:
+
+code
+  | p#myID.firstClass.secondClass Howdy!
+
+p You can skip the tag name for divs:
+
+code
+  | .classy I'm a classy div!
+  br
+  | div I'm a div with no class!
+
+.nodes
+  p
+    ' My coolest feature, though, is this:
+    ' You can create
+    b living dom nodes&nbsp;
+    ' and insert them into a template.
+    ' These dom nodes will retain their event bindings,
+    ' meaning you can declare your events on the node
+    ' before rendering and they'll never get trashed
+    ' in a re-render.
+
+  p == @node()
+
+  p Pretty cool huh?
 """
 
 root.init = ->
-  f = Fragment.parse(template, message: "Hello Interpolation!")
+  f = Fragment.parse template,
+    pipeMessage: "| creates text nodes with no trailing space."
+    aposMessage: "' creates text nodes with a trailing space."
+    node: ->
+      b = document.createElement('button')
+      b.innerText = "Click me and check the console!"
+      b.addEventListener 'click', ->
+        console.log "You clicked my node!"
+      b
+
   document.body.appendChild(f)
